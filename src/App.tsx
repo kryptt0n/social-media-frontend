be3f8a56a3c9526a7d1e92bc202c536d3fc45d9d
Desmond <desmondzkx@gmail.com>
2024-11-01 19:12:39 -0400

    update app route

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ const router = createBrowserRouter([
     element: <ResetFinish />,
   },
   {
-    path:"/",
+    path:"/app",
     element: <Layout />
   }
 ]);
--- src/pages/app/layout.tsx
+++ src/pages/app/layout.tsx
@@ -3,6 +3,9 @@ import { Outlet } from "react-router-dom";
 export default function Layout(){
     return (
         <>
+        <div>
+            <p>this is the layout</p>
+        </div>
             <div>
                 <Outlet />
             </div>
